0df4fd277d64d3ffdb4ba506fa2fb9ef2e78fe94
Gordão-dos-Arcades <66932883+Kayque-Freitas@users.noreply.github.com>
2025-08-06 12:25:02 -0300

    adding things 1

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sql308.infinityfree.com"; // host infinitefree
-$user = "if0_39645844";      // seu usuário MySQL
-$pass = "208KqnHO2d";         // sua senha MySQL
-$db   = "if0_39645844_Cadastro";  // seu banco de dados
+$host = "sql308.infinityfree.com";
+$user = "if0_39645844";      
+$pass = "208KqnHO2d";         
+$db   = "if0_39645844_Cadastro"; 
 
 $conn = new mysqli($host, $user, $pass, $db)
 
